a7967cc1ee3a507debd2e0c2c875d360886b13e6
InancsiKrisztian927 <35177338+InancsiKrisztian927@users.noreply.github.com>
2021-10-22 08:31:06 +0200

    Update egyenletek.py

--- egyenletek.py
+++ egyenletek.py
@@ -1,1 +1,37 @@
 
+egyenletek = ["Elsőfokú","másodfokú"]
+választás = ["első", "másod"]
+
+
+def f1(label):
+    print(label)
+
+
+    prompt = "\tElsőfokú(írd:első), vagy másodfokú(írd:másod) egyenletet szeretnél megoldani? : "
+    első= input(prompt)
+    x = 0
+
+    for i in range(0, len(egyenletek),1):
+        if(egyenletek[i] == első):
+            prompt = "\tAdd meg az a értékét: "
+            number1 = int(input(prompt))
+
+            prompt = "\tAdd meg az b értékét: "
+            number2 = int(input(prompt))
+
+            txt = "\t"+str(number1*x+number2)
+            print(txt)
+            
+            
+        
+
+
+
+
+
+
+
+
+
+
+f1("")
